3687b010dd9ca910309db460eb83f7d3c1ccc571
Steve Dever <stephendever.cr@gmail.com>
2021-03-23 12:51:52 -0400

    add timer and questions/answers as keys/values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,73 @@
-var timerEL = document.getElementById('timer');
-var mainEl = document.getElementById('main');
+var timerEl = document.getElementById('timer');
+var mainEl = document.getElementById('primary');
+var secondaryEl = document.getElementById('secondary');
 var highScore = document.getElementById('highscore');
 var startButton = document.getElementById('sbutton');
 var message = "Game Over!";
-var timeLeft 
+var timeLeft;
+var timeInterval;
+var i;
+var test ="test content";   
 
-// Timer that counts down from 60
-//function countdown () {
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ___________.",
+    choices: ["quotes", "curly brackets", "parantheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {   title: "Arrays in JavaScript can be used to store ___________.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {   title: "String values must be enclosed within ___________ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parantheses"],
+    answer: "quotes"
+  },
+  {   title: "A very useful tool used during development and debugging for printing   content to the debugger is ___________.",
+    choices: ["JavaScrip", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
 
+startButton.addEventListener("click", function () {
+    console.log("Hello, I'm working!");
+    startQuiz();
+});
 
 
 function startQuiz() {
-    timeLeft = 60;
-    //sets timer
-    timer = setInterval(function(){
-    timeLeft--;
-    timerEl.textContent = timeLeft;    
-    },1000);
+    timeLeft = 75;
+    timeInterval = setInterval(function () {
+        console.log(timeLeft)
 
-    startButton.addEventListener("click",function(){
-// event.preventDefault();
-    if (timeLeft > 1) {
-    timerEL.textContent = timeLeft + ' s remaining';
-    console.log(timeLeft);
-         } 
-    else {
-         timerEl.textContent=''
-         clearInterval(timeInterval);
-         displayMessage()   
-         }
-     }, 1000);
-        
-function displayMessage() {
-message;
-}    
-} 
+        if (timeLeft > 0) {
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+        }
+    }, 1000);
+
+        for (i = 1; i < 6; i++) {
+            mainEl.textContent = " ";
+            secondaryEl.textContent = questions(i); // need to "inject" object values here
+         
+             
+    }       
+
+
+
+//    else {
+//         timerEl.textContent=''
+//         clearInterval(timeInterval);
+//         displayMessage()   
+//         }
+//     }, 1000);
+
+// function displayMessage() { ??????????
+// message;
+
+}
