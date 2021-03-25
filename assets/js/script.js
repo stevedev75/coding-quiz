b6dbe7c8b7eab18f533f337b7ca646fc425e7aa1
Steve Dever <stephendever.cr@gmail.com>
2021-03-25 11:37:29 -0400

    change to have correct and matching first questions and answers appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// pseudo-code steps
+// 1 when user clicks start btn, bring up quiz, begin countdown timer 
+// 2 when user chooses an answer, check if right or wrong
+// 3 if answer is right, display "correct" and move to next question
+// 4 if answer is wrong, display "wrong", deduct 10 seconds from timer, 
+//   and move to the next question
+// 5 repeat steps 2 through 4 until finished all five questions
+// 6 when quiz is complete:
+// a. display "game over" 
+// b. stop the clock,
+// c. use the remaining time as "high score", 
+// d. prompt for/collect users initials
+// e. post initials with high score
+
+
+
 var timerEl = document.getElementById('timer');
 var mainEl = document.getElementById('primary');
 var secondaryEl = document.getElementById('secondary');
@@ -7,9 +23,10 @@ var orderedList = document.getElementById('choiceList')
 var message = "Game Over!";
 var timeLeft;
 var timeInterval;
-var i;
-var test ="test content";   
-var html = "";
+var test = "test content";
+var questionIndex = 0;
+
+
 
 var questions = [
   {
@@ -22,15 +39,18 @@ var questions = [
     choices: ["quotes", "curly brackets", "parantheses", "square brackets"],
     answer: "parentheses"
   },
-  {   title: "Arrays in JavaScript can be used to store ___________.",
+  {
+    title: "Arrays in JavaScript can be used to store ___________.",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     answer: "all of the above"
   },
-  {   title: "String values must be enclosed within ___________ when being assigned to variables.",
+  {
+    title: "String values must be enclosed within ___________ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parantheses"],
     answer: "quotes"
   },
-  {   title: "A very useful tool used during development and debugging for printing   content to the debugger is ___________.",
+  {
+    title: "A very useful tool used during development and debugging for printing   content to the debugger is ___________.",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
     answer: "console.log"
   }
@@ -39,40 +59,60 @@ var questions = [
 
 
 startButton.addEventListener("click", function () {
-    console.log("Hello, I'm working!");
-    startQuiz();
+  console.log("Hello, 'start button' working!");
+  startQuiz();
 });
+
+//where you determine if right or wrong
+function choiceClick() {
+  console.log("Hello, 'choice click' is working!");
+
+
+  // will need to add stuff to this function (perhaps "this" goes here??)
+  //if (){
+  // console.log(question.choices);
+}
 
 
 function startQuiz() {
-    timeLeft = 75;
-    timeInterval = setInterval(function () {
-        console.log(timeLeft)
+  timeLeft = 75;
+  timeInterval = setInterval(function () {
+    console.log(timeLeft)
 
-        if (timeLeft > 0) {
-            timerEl.textContent = timeLeft;
-            timeLeft--;
-        }
-    }, 1000);
+    if (timeLeft > 0) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    }
+  }, 1000);
 
-        for (i = 1; i < 6; i++) {
-            mainEl.textContent = " ";
-            secondaryEl.textContent = questions[i].title;
-            orderedList.append(questions[i].choices[i]);
+  var question = questions[questionIndex]
 
-            // orderedList.textContent = questions[i].choices;
-           // need to "inject" object values (choices) here 
-            
-            console.log("i = " + i);
-    }       
-//    else {
-//         timerEl.textContent=''
-//         clearInterval(timeInterval);
-//         displayMessage()   
-//         }
-//     }, 1000);
+  for (var i = 0; i < question.choices.length; i++) {
+    mainEl.textContent = " ";
+    secondaryEl.textContent = question.title;
 
-// function displayMessage() { ??????????
-// message;
+    var choiceBtn = document.createElement("button");
+    choiceBtn.innerHTML = question.choices[i];
+    choiceBtn.className = "button";
+    // Tutor said I'll need to add something here ("this" stuff??)  
+
+    choiceBtn.onclick = choiceClick;
+ 
+
+    orderedList.append(choiceBtn);
+
+    console.log("i = " + i);
+
+    // iterating 4x bc they are 4 answer choices displayed
+    // I need to get an iteration counter for the questions.titles. for
+    // loop to work correction (I think).
+  }
+  //    else {
+  //         timerEl.textContent=''
+  //         clearInterval(timeInterval);
+  //         displayMessage()   
+  //         }
+  //     }, 1000);
+
 
 }
